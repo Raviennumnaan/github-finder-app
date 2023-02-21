7773e562f305a6104612e6120c6cad19aed81a21
Ravishankar M <raviennumnaan@gmail.com>
2023-02-21 13:59:58 +0530

    first commit

---
+++ src/context/github/GithubActions.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+});
+
+export const searchUsers = async function (text) {
+  const params = new URLSearchParams({ q: text });
+
+  const res = await github.get(`/search/users?${params}`);
+  return res.data.items;
+};
+
+export const getUserAndRepos = async function (login) {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ]);
+
+  if (user.status === 404 || repos.status === 404)
+    return (window.location = '/notfound');
+
+  return { user: user.data, repos: repos.data };
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UsersResults from "../components/layout/users/UsersResults";
+import UserSearch from "../components/layout/users/UserSearch";
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UsersResults />
+    </>
+  );
+}
+
+export default Home;
